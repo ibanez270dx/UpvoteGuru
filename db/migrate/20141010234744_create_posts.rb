acfc939f20b6f5ee15cdf4a41d2d8561275c36e2
Jeff Miller <loadeddesigns@gmail.com>
2014-10-10 16:50:38 -0700

    Add name, url, and upvotes (upvote count) attributes to posts and migrate database

--- db/migrate/20141010234744_create_posts.rb
+++ db/migrate/20141010234744_create_posts.rb
@@ -1,7 +1,9 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
-
+      t.string :name
+      t.string :url
+      t.integer :upvotes, default: 0
       t.timestamps
     end
   end
